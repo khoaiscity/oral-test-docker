36d1afa86a38a721c70a6541b40d79d2ca73d2da
datiscity <dat@iscity.com.my>
2021-10-15 10:36:51 +0800

    backend

--- release/run.sh
+++ release/run.sh
@@ -3,24 +3,24 @@
 BUILD_ID=$(date +%b%d-%H%M)
 case $1 in
     "backend")
-    docker-compose --project-name=$BUILD_ID-oral up --no-color --no-start --build $1
-    docker stop $(docker ps -f name=oral_$1 -q)
-    docker-compose --project-name=$BUILD_ID-oral start $1
+    docker-compose --project-name=$BUILD_ID-oral-test up --no-color --no-start --build $1
+    docker stop $(docker ps -f name=oral-test_$1 -q)
+    docker-compose --project-name=$BUILD_ID-oral-test start $1
     ;;
     "admin")
-    docker-compose --project-name=$BUILD_ID-oral up --no-color --no-start --build $1
-    docker stop $(docker ps -f name=oral_$1 -q)
-    docker-compose --project-name=$BUILD_ID-oral start $1
+    docker-compose --project-name=$BUILD_ID-oral-test up --no-color --no-start --build $1
+    docker stop $(docker ps -f name=oral-test_$1 -q)
+    docker-compose --project-name=$BUILD_ID-oral-test start $1
     ;;
     "member")
-    docker-compose --project-name=$BUILD_ID-oral up --no-color --no-start --build $1
-    docker stop $(docker ps -f name=oral_$1 -q)
-    docker-compose --project-name=$BUILD_ID-oral start $1
+    docker-compose --project-name=$BUILD_ID-oral-test up --no-color --no-start --build $1
+    docker stop $(docker ps -f name=oral-test_$1 -q)
+    docker-compose --project-name=$BUILD_ID-oral-test start $1
     ;;
     "resource")
-    docker-compose --project-name=$BUILD_ID-oral up --no-color --no-start --build $1
-    docker stop $(docker ps -f name=oral_$1 -q)
-    docker-compose --project-name=$BUILD_ID-oral start $1
+    docker-compose --project-name=$BUILD_ID-oral-test up --no-color --no-start --build $1
+    docker stop $(docker ps -f name=oral-test_$1 -q)
+    docker-compose --project-name=$BUILD_ID-oral-test start $1
     ;;
     *) echo please provide a service name ;;
 esac
